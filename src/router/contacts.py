6ee00cb4635249af1e08848b43760771c180e23d
Zdorovets Boris <bor.zdorovets28@gmail.com>
2024-09-10 23:44:57 +0300

    змінив структуру

--- src/router/contacts.py
+++ src/router/contacts.py
@@ -1,15 +1,13 @@
-import uvicorn
-
-from fastapi import FastAPI, Depends, HTTPException, status, Query, Path
+from fastapi import Depends, HTTPException, status, Query, APIRouter
 from sqlalchemy.orm import Session
 from typing import List
-from db.db import get_db, Contact
-from schema import ContactCreate, ContactResponse
+from src.db.db import get_db
+from src.db.models import Contact
+from src.schemas import ContactCreate, ContactResponse
 
-app = FastAPI()
+router =  APIRouter(prefix='/tags', tags=["tags"])
 
-
-@app.post("/contacts")
+@router.post("/contacts")
 async def create_note(contact: ContactCreate, db: Session = Depends(get_db)):
     new_contact = Contact(
         first_name=contact.first_name,
@@ -24,13 +22,13 @@ async def create_note(contact: ContactCreate, db: Session = Depends(get_db)):
     return new_contact
 
 
-@app.get("/contacts")
+@router.get("/contacts")
 async def read_contacts(skip: int = 0, limit: int = Query(default=10, le=100, ge=10), db: Session = Depends(get_db)):
     contacts = db.query(Contact).offset(skip).limit(limit).all()
     return contacts
 
 
-@app.get("/contacts/{contact_id}")
+@router.get("/contacts/{contact_id}")
 async def read_contact(contact_id: int, db: Session = Depends(get_db)):
     contact = db.query(Contact).filter(Contact.id == contact_id).first()
     if contact is None:
@@ -43,7 +41,7 @@ async def read_contact(contact_id: int, db: Session = Depends(get_db)):
 # async def 
 # # API повинен мати змогу отримати список контактів з днями народження на найближчі 7 днів.
 
-@app.put("/contacts/{contact_id}")
+@router.put("/contacts/{contact_id}")
 async def update_contact(contact_id: int, contact: ContactCreate, db: Session = Depends(get_db)):
     db_contact = db.query(Contact).filter(Contact.id == contact_id).first()
     if db_contact is None:
@@ -54,7 +52,7 @@ async def update_contact(contact_id: int, contact: ContactCreate, db: Session = 
     db.refresh(db_contact)
     return db_contact
 
-@app.delete("/contacts/{contact_id}", status_code=status.HTTP_204_NO_CONTENT)
+@router.delete("/contacts/{contact_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_contact(contact_id: int, db: Session = Depends(get_db)):
     db_contact = db.query(Contact).filter(Contact.id == contact_id).first()
     if db_contact is None:
@@ -62,13 +60,3 @@ async def delete_contact(contact_id: int, db: Session = Depends(get_db)):
     db.delete(db_contact)
     db.commit()
     return None
-
-
-
-
-
-
-if __name__ == "__main__":
-    uvicorn.run(
-        "main:app", host="0.0.0.0", port=8000,
-    )
